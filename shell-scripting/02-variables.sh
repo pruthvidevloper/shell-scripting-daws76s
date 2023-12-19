d857e26d3d6d6f2a947c00ba924a3471e27cef0a
unknown <pruthviraj5793@gmail.com>
2023-12-20 04:31:05 +0530

    i am practicing variables

--- shell-scripting/02-variables.sh
+++ shell-scripting/02-variables.sh
@@ -3,7 +3,7 @@
 PERSON1:pruthvi
 PERSON2:yamini
 
-echo "PERSON1: how are you"
-echo "PERSON2: i am good"
-echo "PERSON1: what are you doing"
-echo "PERSON2: i am going for a walk"
+echo "$PERSON1: how $PERSON2 are you"
+echo "$PERSON2: i am good, $PERSON1"
+echo "$PERSON1: what are you doing $PERSON2"
+echo "$PERSON2: $PERSON1 , i am going for a walk"
